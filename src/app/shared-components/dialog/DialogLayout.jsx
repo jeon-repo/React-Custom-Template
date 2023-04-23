e9b91b2c991d984aa851656d67bef73a727f97f9
jeon-repo <jano.jeon@gmail.com>
2023-04-23 23:26:47 +0900

    🔨 add dialog on/off

--- src/app/shared-components/dialog/DialogLayout.jsx
+++ src/app/shared-components/dialog/DialogLayout.jsx
@@ -1,11 +1,32 @@
-import { getDialogOptions, getDialogState } from 'app/store/dialogSlice';
-import { useSelector } from 'react-redux';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import { closeDialog, getDialogOptions, getDialogState } from 'app/store/dialogSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 function DialogLayout() {
+  const dispatch = useDispatch();
   const state = useSelector(getDialogState);
   const { content } = useSelector(getDialogOptions);
 
-  return <div style={{ display: state ? 'block' : 'none' }}>{content}</div>;
+  return (
+    <div style={{ display: state ? 'block' : 'none' }}>
+      <div
+        style={{
+          position: 'fixed',
+          backgroundColor: 'transparent',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          zIndex: 0,
+          width: '100vw',
+          height: '100vh',
+        }}
+        onClick={() => dispatch(closeDialog())}
+      />
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default DialogLayout;
